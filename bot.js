1cde731cc16e1a0ef995ddb5882ab6b255f435fa
Gaiji <34226052+Gaiji@users.noreply.github.com>
2018-02-04 11:11:47 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-let prefix = '\'
-
 client.on('ready', () => {
-    console.log('Ok');
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content.startsWith(prefix + 'test')) {
-    	let args = message.content.split(" ").slice(1);
-        let a = args.join(" ")
-        var url = 'https://api.mojang.com/users/profiles/minecraft/'+a
-        request(url, function(err, response, body) {
-            if(!body) {
-                return message.reply('指定されたプレイヤーは存在しません');
-            }
-            body = JSON.parse(body);
-            var url2 = 'http://hypixel.jp:5555/API/'+unk+'.html'
-            request(url2, function(err, response, body) {
-                body = JSON.parse(body);
-                message.reply('Test:'+body.ip):
-            }
-        }
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
 
